187b28910c80bdb7df7f4e06aadae3948ce1a589
JonathanCarey13 <jonathan.carey1995@gmail.com>
2020-11-23 19:45:16 -0500

    second commit

---
+++ RestaurantRater/Models/Restaurant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    public class Restaurant
+    {
+        public int RestaurantID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Rating { get; set; }
+    }
+
+    public class RestaurantDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
