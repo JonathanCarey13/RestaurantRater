0cc6c150b4496e144130b45d9df38a80f16315ec
JonathanCarey13 <jonathan.carey1995@gmail.com>
2020-11-23 20:29:54 -0500

    third commit

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestaurantRater.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,12 @@ namespace RestaurantRater.Controllers
 {
     public class RestaurantController : Controller
     {
-        // GET: Restaurant/Index
+        private RestaurantDbContext _db = new RestaurantDbContext();
+
+        // GET: Restaurant/Index/
         public ActionResult Index()
         {
-            return View();
+            return View(_db.Restaurants.ToList());
         }
     }
 }
