4b5022d3d59940f2be8779aea179facf5c64895b
JonathanCarey13 <jonathan.carey1995@gmail.com>
2020-11-23 19:21:37 -0500

    Add project files.

---
+++ RestaurantRater/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestaurantRater.Startup))]
+namespace RestaurantRater
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
