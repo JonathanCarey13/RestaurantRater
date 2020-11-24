58425db216a3c36587cb7928263677a8986e425e
JonathanCarey13 <jonathan.carey1995@gmail.com>
2020-11-23 19:53:04 -0500

    third commit

---
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantRater.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        // GET: Restaurant/Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
