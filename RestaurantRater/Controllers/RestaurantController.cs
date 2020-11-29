e07c154e59673720a6d7fe571eeba8b1412719f8
JonathanCarey13 <jonathan.carey1995@gmail.com>
2020-11-29 18:52:55 -0500

    crud complete

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -100,5 +100,20 @@ namespace RestaurantRater.Controllers
             return View(restaurant);
         }
 
+        // Get: Restaurant/Details/{id}
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
     }
 }
