68ef9b3a43260b6601385d732eff846a31253c3e
JonathanCarey13 <jonathan.carey1995@gmail.com>
2020-11-29 18:38:47 -0500

    edit works

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RestaurantRater.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -63,6 +64,40 @@ namespace RestaurantRater.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
 
+        }
+
+        // Get: Restaurant/Edit/{id}
+        // Get an id from the user
+        // Handle if the id is null
+        // Find a restaurant by that id
+        // If the restaurant doesn't exist
+        // Return the restaurant and the view
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound(); // I could so write: return new HttpStatusCodeResult(System.Net.HttpStatusCode.NotFound);
+            }
+            return View(restaurant);
+        }
+
+        // Post: Restaurant/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(restaurant).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
         }
 
     }
